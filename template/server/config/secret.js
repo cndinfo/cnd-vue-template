3f0006c3766eb580358c82b71e6108f7836fbf41
jiasen.li@gmail.com <jiasen.li@gmail.com>
2018-04-10 17:32:04 +0800

    修改开发环境和生产环境服务器token的保存方式

--- template/server/config/secret.js
+++ template/server/config/secret.js
@@ -1,43 +1,64 @@
 var crypto = require('crypto')
 
 exports.secretToken = crypto.randomBytes(64).toString('hex')
-const TOKEN_EXPIRATION = 7000
+const TOKEN_EXPIRATION = 6000
 const TOKEN_EXPIRATION_SEC = TOKEN_EXPIRATION * 1000
 
 exports.TOKEN_EXPIRATION = TOKEN_EXPIRATION
 exports.TOKEN_EXPIRATION_SEC = TOKEN_EXPIRATION_SEC
 const cache = require('memory-cache')
+// 开发环境暂时不开启redis
+const redisClient = require('./redis').redisClient
 
 exports.setToken = (token) => {
-  if (token != null) {
-    cache.put(token, token, TOKEN_EXPIRATION_SEC)
+  if (process.env.NODE_ENV === 'production') {
+    redisClient.set(token, '', TOKEN_EXPIRATION_SEC)
+  } else {
+    if (token != null) {
+      cache.put(token, token, TOKEN_EXPIRATION_SEC)
+    }
   }
 }
 
 // Middleware for token verification
 exports.verifyToken = (req, res, next) => {
-  var token = getToken(req)
-  if (cache.get(token)) {
-    next()
+  var token = getToken(req.headers)
+
+  if (process.env.NODE_ENV === 'production') {
+    redisClient.get(token, function(err, reply) {
+      if (err) {
+        console.log(err)
+        return res.send(500)
+      }
+
+      if (reply) {
+        res.send(401)
+      } else {
+        next()
+      }
+    })
   } else {
-    res.sendStatus(401)
+    if (cache.get(token)) {
+      next()
+    } else {
+      return res.sendStatus(401)
+    }
   }
 }
 
-var getToken = (req) => {
-  var token = req.body.token || req.query.token || req.headers['x-access-token']
-  if (req.headers && req.headers.authorization) {
-    var authorization = req.headers.authorization
+var getToken = (headers) => {
+  if (headers && headers.authorization) {
+    var authorization = headers.authorization
     var part = authorization.split(' ')
 
     if (part.length === 2) {
-      token = part[1]
+      var token = part[1]
 
       return token
     } else {
       return null
     }
-  } else if (token) {
-    return token
+  } else {
+    return null
   }
 }
